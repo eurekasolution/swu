df0fb00455ad7b78625356b1fad1aac8352ba28d
eurekasolution <kjmin@daum.net>
2024-08-20 15:39:29 +0900

    A1540

--- 02.php
+++ 02.php
@@ -8,7 +8,7 @@ function connectDB() {
     // MySQL 데이터베이스 연결
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // 연결 확인
+    // 연결 확인 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
